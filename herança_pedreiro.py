dc0a15604bb7b664a0e289cac0650ce22b44c530
eppodk <135907710+eppodk@users.noreply.github.com>
2023-06-20 14:23:11 -0300

    Update herança_pedreiro.py

--- herança_pedreiro.py
+++ herança_pedreiro.py
@@ -3,4 +3,4 @@ class Pedreiro(Funcionario):
     def __init__(self,nome, cpf, fone, salario):
         super().__init__(nome)
         self.cpf = cpf
-        self.cargo=("Pedreiro")
+        self.cargo = ("Pedreiro")
