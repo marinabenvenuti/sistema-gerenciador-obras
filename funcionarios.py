b65e86eef35b10d032f51ea2091bf6e95c8a79f6
eppodk <135907710+eppodk@users.noreply.github.com>
2023-06-20 14:34:35 -0300

    Update funcionarios.py

--- funcionarios.py
+++ funcionarios.py
@@ -1,7 +1,8 @@
 class Funcionario:
-    def __init__(self, nome, cpf, fone, salario):
+    def __init__(self, nome, cpf, fone, salario, cadastro):
         self.nome = nome
         self.cpf = cpf
         self.fone = fone
         self.salario = salario
+        self.cadastro = cadastro
         
