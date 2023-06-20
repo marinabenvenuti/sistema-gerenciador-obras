7f92e8e253c398bd3ecf7ad953f8c64cd655f031
marina <102808736+marinabenvenuti@users.noreply.github.com>
2023-06-20 14:51:12 -0300

    Update herança_pedreiro.py

--- herança_pedreiro.py
+++ herança_pedreiro.py
@@ -1,7 +1,7 @@
 from funcionarios import funcionario
 class Pedreiro(Funcionario):
-    def __init__(self, nome, cpf, fone, salario, qtdObras):
-        super().__init__(nome, cpf, fone, salario)
+    def __init__(self, nome, sobrenome, cpf, fone, salario, qtdObras):
+        super().__init__(nome, sobrenome, cpf, fone, salario)
         self.qtdObras = qtdObras
         
     def getQtdObras(self):
