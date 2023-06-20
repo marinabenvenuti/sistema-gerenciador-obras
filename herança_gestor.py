81a24148c250f4d0147ff89e5c70e0c8b1a80f0e
marina <102808736+marinabenvenuti@users.noreply.github.com>
2023-06-20 14:29:07 -0300

    Update herança_gestor.py

--- herança_gestor.py
+++ herança_gestor.py
@@ -1,6 +1,11 @@
 from funcionarios import funcionario
 class Gestor(Funcionario):
-    def __init__(self,nome, cpf, fone, salario):
-        super().__init__(nome)
-        self.cpf = cpf
-        self.cargo = ("Gestor")
+    def __init__(self, nome, cpf, fone, salario, dataContratacao):
+        super().__init__(nome, cpf, fone, salario)
+        self.dataContratacao = dataContratacao
+        
+    def getDataContratacao(self):
+        return self.dataContratacao
+    
+    def setDataContratacao(self, dataContratacao):
+        self.dataContratacao = dataContratacao
