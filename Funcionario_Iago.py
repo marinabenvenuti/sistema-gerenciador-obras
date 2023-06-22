93963d3a423e601877a4289d27e1d5c5e211ff7f
eppodk <135907710+eppodk@users.noreply.github.com>
2023-06-22 19:17:52 -0300

    Update Funcionario_Iago.py

--- Funcionario_Iago.py
+++ Funcionario_Iago.py
@@ -4,9 +4,8 @@ pedreiros = []
 gestores = []
 
 class Funcionario:
-    def __init__(self, Fun_Nome, Fun_Sobrenome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
         self.nome = Fun_Nome
-        self.sobrenome = Fun_Sobrenome
         self.cpf = Fun_CPF
         self.fone = Fun_Fone
         self.cadastro = Fun_Cadastro
@@ -15,18 +14,19 @@ class Funcionario:
         
 class Gestor(Funcionario):
     
-    def __init__(self, Fun_Nome, Fun_Sobrenome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
-        super().__init__(Fun_Nome, Fun_Sobrenome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
+        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
         
         self.contra= int(input('Data da contrataçao[AAAA]: '))
         self.Sal_Calc=(self.salario+(500*(2023-self.contra)))
         gestores.append(self)
+        funcionarios.append(self)
         
 class Pedreiro(Funcionario):
-    def __init__(self, Fun_Nome, Fun_Sobrenome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
-        super().__init__(Fun_Nome, Fun_Sobrenome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
+        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
         
         self.NumObras=0
-        self.Sal_Calc=(self.salario+(350*(2023-self.NumObras)))
+        self.Sal_Calc=(self.salario+(350*(self.NumObras)))
         funcionarios.append(self)
         pedreiros.append(self)
