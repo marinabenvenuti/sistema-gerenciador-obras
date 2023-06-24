47024f636bd9987972082cd309b035fab958c171
eppodk <135907710+eppodk@users.noreply.github.com>
2023-06-24 17:12:27 -0300

    Update obras.py

--- obras.py
+++ obras.py
@@ -1,11 +1,9 @@
-from herança_pedreiro import Pedreiro
-salario_pedreiros = 1500 #melhorar essa variável, talvez pedir pra o usuario digitar quanto vai ganhar o pedreiro por obra
-    
 class Obra:
-    def __init__(self, cod, cliente, materiais, pedreiros, dataIn, dataFim, total):
+    def __init__(self, cod, cliente, materiaisusados, pedreiros, dataIn, dataFim, total):
         self.cod = cod
         self.cliente = cliente
         self.materiais = materiais
+        self.total = total
         for i in range (len(self.materias)):
             print("O material", self.materiais[i]['nome'], "possui", self.materiais[i]['qtd'], "itens")
             add_num_mat=int(input("Quantos voce deseja adicionar? a medida é em {} com o valor de R${}".format(self.materiais[i]['medição'], self.materiais[i]['preço'])))
@@ -15,7 +13,7 @@ class Obra:
         self.pedreiros = pedreiros
         self.dataIn = dataIn
         self.dataFim = dataFim
-        self.total = total
+        
         
     def getCod(self):
         return self.cod
