0716c3d63cc10467a26ac6173fb9dcdb920a7476
marina <102808736+marinabenvenuti@users.noreply.github.com>
2023-06-24 19:19:53 -0300

    Update obras.py

--- obras.py
+++ obras.py
@@ -1,19 +1,18 @@
+from funcionarios import *
+obras = []
 class Obra:
-    def __init__(self, cod, cliente, materiaisusados, pedreiros, dataIn, dataFim, total):
+    def __init__(self, cod, cliente, materiais, nomePedr, dataIn, dataFim, total):
         self.cod = cod
         self.cliente = cliente
         self.materiais = materiais
         self.total = total
-        for i in range (len(self.materias)):
-            print("O material", self.materiais[i]['nome'], "possui", self.materiais[i]['qtd'], "itens")
-            add_num_mat=int(input("Quantos voce deseja adicionar? a medida é em {} com o valor de R${}".format(self.materiais[i]['medição'], self.materiais[i]['preço'])))
-            self.materiais[i]['qtd']+=add_num_mat
-            self.total+=(add_num_mat*self.materiais[i]['preço'])
             
-        self.pedreiros = pedreiros
+        self.pedreiro = nomePedr
         self.dataIn = dataIn
         self.dataFim = dataFim
-            
+        obras.append(self)
+        
+        
     def getCod(self):
         return self.cod
     
@@ -34,16 +33,28 @@ class Obra:
             self.materiais[i]['qtd']+=add_num_mat
             self.total+=(add_num_mat*self.materiais[i]['preço'])
             
-        return materiaisusados
-                
-    def setMateriais(self, materiaisusados):
-        self.materiais = materiaisusados
-        
+                            
+    def setMateriais(self, materiais):
+        self.materiais = materiais
+            
+    def setPedreiro(self, nomePedr):
+        self.pedreiro = nomePedr
+            
     def setDataIn(self, dataIn):
         self.dataIn = dataIn
+        
+    def setCod(self, cod):
+        self.cod = cod
             
     def setDataFim(self, dataFim):
         self.dataFim = dataFim
         
+    def calculaObra(self, materiais):
+        total = 350*1.22
+        for i in materiais:
+            total += material['qtd'] * material['preço']
+                
+        return total
+    
     def setTotal(self, totalObra):
         self.total = totalObra
