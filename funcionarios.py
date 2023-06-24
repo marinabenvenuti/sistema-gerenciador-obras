48a36462d4938035b9e84f2f683ce2e7b0048102
marina <102808736+marinabenvenuti@users.noreply.github.com>
2023-06-24 17:01:45 -0300

    Update funcionarios.py

--- funcionarios.py
+++ funcionarios.py
@@ -4,12 +4,12 @@ pedreiros = []
 gestores = []
 
 class Funcionario:
-    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, salarioTot):
         self.nome = Fun_Nome
         self.cpf = Fun_CPF
         self.fone = Fun_Fone
         self.cadastro = Fun_Cadastro
-        self.salario = Fun_Salario
+        self.salario = salarioTot
         
     def setNome(self, nome):
         self.nome = nome
@@ -23,23 +23,33 @@ class Funcionario:
     def setCadastro(self, cadastro):
         self.cadastro = cadastro
         
+    def calculaSalario(self, salario):
+        
     
         
-class Gestor(Funcionario):
-    
-    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
-        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
-        
-        self.contra= int(input('Ano da contratação[AAAA]: '))
-        self.Sal_Calc=(self.salario+(500*(2023-self.contra)))
+class Gestor(Funcionario):    
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, salarioTot, contra):
+        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, salarioTot)
+        self.contra= anoContrat
         gestores.append(self)
         funcionarios.append(self)
         
-class Pedreiro(Funcionario):
-    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
-        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
+    def calculaSalario(self, salario):
+        salarioTot = salario+(500*(2023-self.contra))
+        return salarioTot
         
+        
+    def setContra(self, contratacao):
+        self.contra = contratacao
+        
+class Pedreiro(Funcionario):
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, salarioTot):
+        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, salarioTot)        
         self.NumObras=0
-        self.Sal_Calc=(self.salario+(350*(self.NumObras)))
         funcionarios.append(self)
         pedreiros.append(self)
+        
+    def calculaSalario(self, salario):
+        salarioTot = salario+(350*(self.NumObras))
+        return salarioTot
+        
