e2ea6b4059c1fd3e54be1db27916a250ff786ddb
eppodk <135907710+eppodk@users.noreply.github.com>
2023-06-28 16:22:25 -0300

    Add files via upload

---
+++ funcionariosontem.py
@@ -0,0 +1,62 @@
+funcionarios= []
+pedreiros = []
+gestores = []
+
+class Funcionario:
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario):
+        self.nome = Fun_Nome
+        self.cpf = Fun_CPF
+        self.fone = Fun_Fone
+        self.cadastro = Fun_Cadastro
+        self.salario = Fun_Salario
+        
+    def setNome(self, nome):
+        self.nome = nome
+    
+    def setCPF(self, cpf):
+        self.cpf = cpf
+        
+    def setFone(self, fone):
+        self.fone = fone
+        
+    def setCadastro(self, cadastro):
+        self.cadastro = cadastro
+        
+    def calculaSalario(self, salario):
+        print(f'O salário básico é R${salario}')
+        
+    def setSalario(self, salarioTot):
+        self.salario = salarioTot
+                
+class Gestor(Funcionario):    
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario, anoContrat):
+        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)
+        self.anoContrat = anoContrat
+        gestores.append(self)
+        funcionarios.append(self)
+        
+    def setContra(self, contratacao):
+        self.anoContrat = contratacao
+        
+    def calculaSalario(self, salario, anoContrat):
+        salarioTot = salario+(500*(2023-self.anoContrat))
+        return salarioTot
+    
+    def setSalario(self, salarioTot):
+        self.salario = salarioTot
+        
+class Pedreiro(Funcionario):
+    def __init__(self, Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario, numObras):
+        super().__init__(Fun_Nome, Fun_CPF, Fun_Fone, Fun_Cadastro, Fun_Salario)        
+        self.numObras= numObras
+        funcionarios.append(self)
+        pedreiros.append(self)
+        
+    def calculaSalario(self):
+        self.salario = 1500+(350*(self.numObras))
+        
+    def setNumObras(self):
+        self.numObras += 1
+        
+    def setNumObrasBosta(self):
+        self.numObras -= 1      
---
+++ obrasontem.py
@@ -0,0 +1,35 @@
+from funcionarios import *
+obras = []
+class Obra:
+    def __init__(self, cod, cliente, materiais, total, nomePedr, dataIn, dataFim):
+        self.cod = cod
+        self.cliente = cliente
+        self.materiais = materiais
+        self.total = 0 
+        self.pedreiro = nomePedr
+        self.dataIn = dataIn
+        self.dataFim = dataFim
+                  
+    def setCliente(self, cliente):
+        self.cliente = cliente
+
+    def setMateriais(self, materiais):
+        self.materiais = materiais
+            
+    def setPedreiro(self, nomePedr):
+        self.pedreiro = nomePedr
+            
+    def setDataIn(self, dataIn):
+        self.dataIn = dataIn
+        
+    def setCod(self, cod):
+        self.cod = cod
+            
+    def setDataFim(self, dataFim):
+        self.dataFim = dataFim
+        
+    def setTotalRem(self, totalObra):
+        self.total -= totalObra
+        
+    def setTotal(self, totalObra):
+        self.total += totalObra
